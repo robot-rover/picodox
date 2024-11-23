487d90e1a37fa66dd1a848d6e8edfaba8eb1e8cf
Samuel O'Brien <sam.obrien00@gmail.com>
2024-11-23 16:11:00 -0600

    Basic cli and proto

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,77 @@
+use std::time::Duration;
+
+use anyhow::{Context, Result};
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "picodox-cli")]
+#[command(about = "A cli for interacting with the picodox keyboard")]
+struct Cli {
+    #[arg(help = "The serial port connected to the keyboard")]
+    #[arg(short, long)]
+    // TODO: Read this from a .env file instead?
+    #[arg(default_value_t = String::from("/dev/ttyACM0"))]
+    device: String,
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    #[command(about = "Reset the keyboard mcu")]
+    Reset,
+    #[command(name = "flash")]
+    #[command(about = "Change the keyboard mcu into DFU flash mode")]
+    FlashFw,
+    #[command(about = "List all serial ports")]
+    ListSerial,
+    #[command(about = "Send data to the mcu over serial and read its response")]
+    Serial {
+        #[arg(help = "The content to send")]
+        msg: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Command::Reset => println!("Reset!"),
+        Command::FlashFw => println!("FlashFw!"),
+        Command::ListSerial => list_serial().unwrap(),
+        Command::Serial { msg } => send_serial(&args.device, msg.as_bytes()).unwrap(),
+    };
+}
+
+fn list_serial() -> Result<()> {
+    let ports = serialport::available_ports()
+        .context("Unable to enumerate available serial ports")?;
+    if ports.len() > 0 {
+        for port in ports {
+            println!("{}", port.port_name);
+        }
+    } else {
+        println!("No serial ports detected!");
+    }
+
+    Ok(())
+}
+
+fn send_serial(dev: &str, content: &[u8]) -> Result<()> {
+    let mut port = serialport::new(dev, 115_200)
+        .timeout(Duration::from_millis(100))
+        .open().with_context(|| format!("Failed to open serial port '{dev}'"))?;
+
+    println!("Sending '{}'", String::from_utf8_lossy(content));
+    port.write_all(content)
+        .and_then(|_| port.write(b"\r"))
+        .context("Failed to write to serial port")?;
+
+    let mut read_buf = vec![0u8; 64];
+    let bytes_recv = port.read(&mut read_buf).context("Failed to read from serial port")?;
+    read_buf.truncate(bytes_recv);
+    let read_content = String::from_utf8_lossy(&read_buf);
+    println!("Received response: '{read_content}'");
+
+    Ok(())
 }
--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -11,16 +11,19 @@ pub const CURRENT_VERSION: Version = Version {
     minor: 0,
 };
 
+#[derive(Serialize, Deserialize)]
 pub enum Command {
     Reset,
     FlashFw,
 }
 
+#[derive(Serialize, Deserialize)]
 pub enum Response {
     LogMsg {
         bytes_count: u16,
     },
     Data([u8; 8]),
+    PacketErr,
 }
 
 
@@ -28,13 +31,17 @@ pub enum Response {
 mod tests {
     use super::*;
 
-    use postcard::to_vec;
+    use postcard::to_stdvec;
 
     #[test]
     fn check_enum_size() {
         let short = Response::LogMsg { bytes_count: 12 };
-        let short_bytes = to_vec(&short);
+        let short_bytes = to_stdvec(&short)
+            .expect("Cannot serialize short response");
         let long = Response::Data([1u8; 8]);
-        let long_bytes = to_vec(&long);
+        let long_bytes = to_stdvec(&long)
+            .expect("Cannot serialize long response");
+        assert_eq!(short_bytes.len(), 2);
+        assert_eq!(long_bytes.len(), 9);
     }
 }
