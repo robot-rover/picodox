c6f7289f14e5cf809c77c11728145b4893263036
Samuel O'Brien <sam.obrien00@gmail.com>
2024-11-26 10:08:33 -0600

    Progress: move to embassy

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -10,110 +10,110 @@
 #![no_std]
 #![no_main]
 
-mod serial;
-mod proto_impl;
+//mod serial;
+//mod proto_impl;
 
-use adafruit_kb2040 as board;
-
-// The macro for our start-up function
-use board::entry;
-
-// Ensure we halt the program on panic (if we don't mention this crate it won't
-// be linked)
 use panic_halt as _;
 
-// Pull in any important traits
+use embassy_executor::Spawner;
+use embassy_usb::driver::EndpointError;
+use embassy_usb::{Builder, Config, UsbDevice, UsbDeviceState};
+use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use embassy_rp::usb::{self, Driver};
+use embassy_rp::peripherals::USB;
+use embassy_rp::bind_interrupts;
+use static_cell::StaticCell;
 
-// A shorter alias for the Peripheral Access Crate, which provides low-level
-// register access
-use board::hal::pac;
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => usb::InterruptHandler<USB>;
+});
 
-// A shorter alias for the Hardware Abstraction Layer, which provides
-// higher-level drivers.
-use board::hal;
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
 
-// USB Device support
-use usb_device::{class_prelude::*, prelude::*};
+    // Create the driver, from the HAL.
+    let driver = usb::Driver::new(p.USB, Irqs);
 
-/// Entry point to our bare-metal application.
-///
-/// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
-/// as soon as all global variables are initialised.
-///
-/// The function configures the RP2040 peripherals, then blinks the LED in an
-/// infinite loop.
-#[entry]
-fn main() -> ! {
-    // Grab our singleton objects
-    let mut pac = pac::Peripherals::take().unwrap();
-    let _core = pac::CorePeripherals::take().unwrap();
+    // Create embassy-usb Config
+    let config = {
+        const USB_VID : u16 = 0x08B9;
+        const USB_PID : u16 = 0xBEEF;
 
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+        let mut config = Config::new(USB_VID, USB_PID);
+        config.device_class = 0; // from: https://www.usb.org/defined-class-codes
+        config.manufacturer = Some("rr Industries");
+        config.product = Some("Picodox Keyboard");
+        config.serial_number = Some("0000-0001");
+        config.max_power = 100; // mA
+        config.max_packet_size_0 = 64;
 
-    // Configure the clocks
-    //
-    // The default is to generate a 125 MHz system clock
-    let clocks = hal::clocks::init_clocks_and_plls(
-        board::XOSC_CRYSTAL_FREQ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
+        config
+    };
 
-    // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(pac.SIO);
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut builder = {
+        static CONFIG_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+        static BOS_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+        static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
 
-    // Set the pins up according to their function on this particular board
-    let _pins = board::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
+        let builder = embassy_usb::Builder::new(
+            driver,
+            config,
+            CONFIG_DESCRIPTOR.init([0; 256]),
+            BOS_DESCRIPTOR.init([0; 256]),
+            &mut [], // no msos descriptors
+            CONTROL_BUF.init([0; 64]),
+        );
+        builder
+    };
 
-    // Set the LED to be an output
-    //let _led_pin = pins.led.into_push_pull_output();
+    // Create classes on the builder.
+    let mut class = {
+        static STATE: StaticCell<State> = StaticCell::new();
+        let state = STATE.init(State::new());
+        CdcAcmClass::new(&mut builder, state, 64)
+    };
 
-    // Blink the LED at 1 Hz
-    // loop {
-    //     led_pin.set_high().unwrap();
-    //     delay.delay_ms(500);
-    //     led_pin.set_low().unwrap();
-    //     delay.delay_ms(500);
-    // }
-    //
-    //let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+    // Build the builder.
+    let usb = builder.build();
 
-    // Set up the USB driver
-    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
-        pac.USBCTRL_REGS,
-        pac.USBCTRL_DPRAM,
-        clocks.usb_clock,
-        true,
-        &mut pac.RESETS,
-    ));
+    // Run the USB device.
+    let _unit = if let Ok(unit) = spawner.spawn(usb_task(usb)) {
+        unit
+    } else {
+        panic!();
+    };
 
-    // Set up the USB Communications Class Device driver
-
-    let mut serial = serial::SerialIf::setup(&usb_bus);
-
-    // Create a USB device with a fake VID and PID
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x08B9, 0xBEEF))
-        .strings(&[StringDescriptors::default()
-            .manufacturer("rr Industries")
-            .product("Picodox Keyboard")
-            .serial_number("0000-0001")])
-        .unwrap()
-        .device_class(0) // from: https://www.usb.org/defined-class-codes
-        .build();
-
+    // Do stuff with the class!
     loop {
-        serial.poll(&mut usb_dev);
+        class.wait_connection().await;
+        let _ = echo(&mut class).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn usb_task(mut usb: UsbDevice<'static, Driver<'static, USB>>) -> ! {
+    usb.run().await
+}
+
+struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+async fn echo<'d, T: usb::Instance + 'd>(class: &mut CdcAcmClass<'d, Driver<'d, T>>) -> Result<(), Disconnected> {
+    let mut buf = [0; 64];
+    loop {
+        let n = class.read_packet(&mut buf).await?;
+        let data = &buf[..n];
+        class.write_packet(data).await?;
     }
 }
