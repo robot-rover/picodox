6cd92cd65d71ac3989886430cdb4741c27abdabd
Sam O'Brien <sam.obrien@ni.com>
2025-02-17 15:54:31 -0600

    keymap changes

--- firmware/src/key_map.rs
+++ firmware/src/key_map.rs
@@ -13,7 +13,7 @@ const LEFT_KEY_MATRIX: [[Key; NUM_COLS]; NUM_ROWS] = [
     // K8-K14
     [KEY_LEFTBRACE, KEY_T, KEY_R, KEY_E, KEY_W, KEY_Q, KEY_TAB],
     // K15-K21
-    [KEY_PAGEUP, KEY_G, KEY_F, KEY_D, KEY_S, KEY_A, KEY_BACKSPACE],
+    [KEY_PAGEUP, KEY_G, KEY_F, KEY_D, KEY_S, KEY_A, KEY_ESC],
     // K22-K28
     [
         KEY_PAGEDOWN,
@@ -22,31 +22,31 @@ const LEFT_KEY_MATRIX: [[Key; NUM_COLS]; NUM_ROWS] = [
         KEY_C,
         KEY_X,
         KEY_Z,
-        KEY_MOD_LSHIFT,
+        KEY_NONE,
     ],
     // K29-K35
     [
-        KEY_DELETE,
-        KEY_ESC,
+        KEY_MOD_LSHIFT,
+        KEY_BACKSPACE,
         KEY_MOD_LCTRL,
         KEY_MOD_LALT,
-        KEY_EQUAL,
-        KEY_MINUS,
+        KEY_NONE,
+        KEY_NONE,
         KEY_MOD_LMETA,
     ],
 ];
 
 const RIGHT_KEY_MATRIX: [[Key; NUM_COLS]; NUM_ROWS] = [
     // K1-K7
-    [KEY_NONE, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, KEY_NONE],
+    [KEY_NONE, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, KEY_EQUAL],
     // K8-K14
-    [KEY_RIGHTBRACE, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, KEY_NONE],
+    [KEY_RIGHTBRACE, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, KEY_MINUS],
     // K15-K21
     [KEY_END, KEY_H, KEY_J, KEY_K, KEY_L, KEY_SEMICOLON, KEY_APOSTROPHE],
     // K22-K28
-    [KEY_HOME, KEY_N, KEY_M, KEY_COMMA, KEY_DOT, KEY_SLASH, KEY_MOD_RSHIFT],
+    [KEY_HOME, KEY_N, KEY_M, KEY_COMMA, KEY_DOT, KEY_SLASH, KEY_NONE],
     // K29-K35
-    [KEY_ENTER, KEY_SPACE, KEY_MOD_RALT, KEY_LEFT, KEY_DOWN, KEY_UP, KEY_RIGHT],
+    [KEY_ENTER, KEY_SPACE, KEY_NONE, KEY_LEFT, KEY_DOWN, KEY_UP, KEY_RIGHT],
 ];
 
 pub struct BasicKeymap {
